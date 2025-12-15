058dcb2f91ffac94e6b7de24777a9b2b28753412
bermood <tonylodygin123@gmail.com>
2025-12-15 14:07:21 +0300

    update BlocksMatrix

--- BlocksMatrix.h
+++ BlocksMatrix.h
@@ -1,22 +1,26 @@
 #ifndef ARKANOID_BLOCKSMATRIX_H
 #define ARKANOID_BLOCKSMATRIX_H
 
+#include "Block.h"
 #include <vector>
 
-class Block;
 
 class BlocksMatrix {
 public:
-    BlocksMatrix(int height, int width);
+    BlocksMatrix(int matrixHeight, int matrixWidth);
     Block* get_block(int x, int y) const;
-    int get_height() const;
-    int get_width() const;
+    int getMatrixHeight() const;
+    int getMatrixWidth() const;
+    int getBlockHeight() const;
+    int getBlockWidth() const;
     void set_block(int x, int y);
     void del_block(int x, int y);
 
 private:
-    int height;
-    int width;
+    int matrixHeight;
+    int matrixWidth;
+    int blockHeight;
+    int blockWidth;
     std::vector<std::vector<Block*>> field;
     void generate();
 };
